37a1c46a18b1c7ab71099a3b4f3711ea88089bdb
anushka2300 <122161237+anushka2300@users.noreply.github.com>
2025-01-26 18:18:06 +0530

    rent

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./home/Dashboard";
-
+import Rent from "./rental/rent";
+import RentProduct from './rental/receiveRent'
+import Description from "./rental/description";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/rent" element={<Rent />} />
+        <Route path="/receive" element={<RentProduct />} />
+        <Route path="/description" element={<Description />} />
       </Routes>
     </Router>
   );
