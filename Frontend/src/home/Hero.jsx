2d41402f7817a7bb3acba102434da98528d4d52a
Rudrika Raghav <116345691+rudrika2425@users.noreply.github.com>
2025-01-26 18:30:35 +0530

    hero.jsx

--- Frontend/src/home/Hero.jsx
+++ Frontend/src/home/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = ({ id }) => {
           backgroundPosition: "center", 
         }}
       ></div>
-<div className="absolute top-0 left-0 w-full h-full bg-black opacity-10"></div>
+<div className="absolute top-0 left-0 w-full h-full bg-black opacity-0"></div>
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center">
         <h1 className="text-6xl font-extrabold text-black mb-6 drop-shadow-lg -mt- mb-10">
           AGRICULTURAL COMMUNITY SERVICE
@@ -22,7 +22,7 @@ const Hero = ({ id }) => {
         <p className="text-4xl text-black  font-bold mb-14 drop-shadow-md mt-3">
           Bringing innovative solutions to farming communities.
         </p>
-        <div className="flex flex-row gap-10 -ml-6">
+        <div className="flex flex-row gap-10 -ml-10">
           <a
             href="#learn-more"
             className="bg-green-600 hover:bg-green-700 text-white py-3 px-6 rounded-lg text-lg font-medium drop-shadow-md"
