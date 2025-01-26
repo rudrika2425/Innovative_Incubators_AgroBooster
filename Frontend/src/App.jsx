40f90ed00c6a0b4598d518c293a02b0ee203ef21
Rudrika Raghav <116345691+rudrika2425@users.noreply.github.com>
2025-01-26 13:40:10 +0530

    Router installed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./home/Dashboard";
 
 
 function App() {
-  
-
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
