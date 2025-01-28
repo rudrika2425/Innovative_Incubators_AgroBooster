a8bd67d607ed5541a0b831edbf014a42cad958c4
akshayyadav63 <akshay.kumar_cs22@gla.ac.in>
2025-01-28 21:10:24 +0530

    updated app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,8 +7,10 @@ import Description from "./rental/description";
 import Login from "./crop/Login";
 import Signup from "./crop/Signup";
 import SoilTestReportUploader from "./dashboard/test";
-
-
+import FarmerDashboard from "./dashboard/DashboardFarmer";
+import Chatbot from "./ChatBoat/Chatboat"
+import BasicInfoForm from "./dashboard/BasicInfoForm";
+import SoilTestForm from "./dashboard/SoilTestForm";
 
 function App() {
  
@@ -22,6 +24,7 @@ function App() {
         <Route path="/description" element={<Description />} /> 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />}/>
+        <Route path="/chatbot" element={<Chatbot />} />
         <Route path="/test" element={<SoilTestReportUploader />}/>
         <Route path="/farmer-Information" element={<FarmerDashboard />}>
           <Route path="step1" element={<BasicInfoForm />} />
