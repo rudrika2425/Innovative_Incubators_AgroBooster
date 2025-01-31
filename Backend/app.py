286f39003c08d1ce4661dff30088f584d07f7857
Devansh Agrawal <122165428+devanshcs22@users.noreply.github.com>
2025-01-31 22:26:54 +0530

    commit

--- Backend/app.py
+++ Backend/app.py
@@ -5,8 +5,6 @@ from flask_pymongo import PyMongo
 from config import Config
 from routes.auth_routes import auth_bp
 from routes.twilio_routes import twilio_bp
-
-from routes.user_routes import user_routes 
 from routes.analyze_soil_report_routes import analyze_soil_report_bp
 from routes.get_location import location_bp
 from routes.weather_routes import weather_bp
@@ -24,7 +22,6 @@ def create_app():
 
     app.register_blueprint(auth_bp, url_prefix="/user")
     app.register_blueprint(twilio_bp, url_prefix="/twilio")
-    app.register_blueprint(user_routes, url_prefix='/users')
     app.register_blueprint(analyze_soil_report_bp, url_prefix='/analyze_soil')  
     app.register_blueprint(location_bp,url_prefix='/location')
     app.register_blueprint(weather_bp,url_prefix='/weather')
