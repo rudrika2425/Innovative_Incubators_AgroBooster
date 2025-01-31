bf9f4fd52fa5d6becec386ffc7e39b15f2b4be7d
akshayyadav63 <akshay.kumar_cs22@gla.ac.in>
2025-01-31 17:00:30 +0530

    chabot done

--- ChatBoat/utils/translate.py
+++ ChatBoat/utils/translate.py
@@ -3,5 +3,12 @@ from googletrans import Translator
 translator = Translator()
 
 def translate_text(text, target_language):
-    translation = translator.translate(text, dest=target_language)
-    return translation.text
+    try:
+        if target_language == "en":  # No need to translate if it's already English
+            return text
+
+        translation = translator.translate(text, dest=target_language)
+        return translation.text
+    except Exception as e:
+        print(f"Translation error: {str(e)}")
+        return text  # Return original text if translation fails
